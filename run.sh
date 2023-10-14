b9d1578022591573fdcbaaea8740c7a6231692dc
florentinobenedictus <florentinobenedictus@gmail.com>
2023-10-14 16:23:23 +0700

    add cpuset

--- run.sh
+++ run.sh
@@ -16,6 +16,11 @@ for cc in {1,10}; do
 for c in {1,5,10,15,20}; do
 for r in {2,3,5,7}; do
 for v in {0,2}; do
+  # cpuset setup
+  sudo cgdelete -g cpuset:/cpulimitf
+  sudo cgcreate -t $USER:$USER -a $USER:$USER -g cpuset:/cpulimitf
+  sudo cgset -r cpuset.mems=0 cpulimitf
+  sudo cgset -r cpuset.cpus=0 cpulimitf
 
   date
   title="${c}c1s${r}r1p_${cc}-con"
@@ -26,7 +31,7 @@ for v in {0,2}; do
     title="${title}_crpc-ring"
   fi
   echo -e "$title\n" > stats/.pid
-  time ./build/deptran_server -f config/test_toy.yml -f config/${c}c1s${r}r1p.yml -f config/rw.yml -f config/concurrent_${cc}.yml -P localhost -v $v -d $duration | tee -a stats/.pid | grep "tid of leader is "
+  time ./build/deptran_server -f config/sample_crpc.yml -f config/${c}c1s${r}r1p.yml -f config/rw.yml -f config/concurrent_${cc}.yml -P localhost -v $v -d $duration | tee -a stats/.pid | grep  --line-buffered "tid of leader is " | sed --unbuffered 's/.*leader is \([0-9]\+\).*/\1/' | sudo tee /sys/fs/cgroup/cpuset/cpulimitf/tasks
   grep "all clients have shut down" stats/.pid
   while [[ $(tail -n 1 stats/summary.log | grep $title | wc -l) -eq 0 ]]; do sleep 2; done
 
