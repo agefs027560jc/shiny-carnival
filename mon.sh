480a37683121c8de12003dd5cb0f75bc5d89912b
Ubuntu <test@no.email>
2023-10-14 04:22:30 +0000

    modes

--- mon.sh
+++ mon.sh
@@ -8,28 +8,32 @@ touch stats/summary.log
 
 # get 1/3 of duration
 d=$(( $(grep "duration=" run.sh | cut -d = -f 2) / 3 ))
+chk=$1
 while :; do
   sleep 2
   title=$(head -n 1 stats/.pid)
   pid=$(grep "tid of leader is " stats/.pid | awk '{print $NF}')
 
   if [[ $(pgrep -c deptran) -gt 0 ]] && [[ $pid -ne 0 ]] && [[ $pid -ne $prevpid ]]; then
-    sleep $d
-    top -H -b -n $d -d 1 -p $pid > stats/top.log
-    grep "$pid" stats/top.log > stats/.avg
-    line=$(grep -c "$pid" stats/.avg)
+    if [[ $chk == "top" ]]; then
+      sleep $d
+      top -H -b -n $d -d 1 -p $pid > stats/top.log
+      grep "$pid" stats/top.log > stats/.avg
+      line=$(grep -c "$pid" stats/.avg)
 
-    calc="((0"
-    for ((i=1; i <= $line; i++)); do
-      x=$(awk 'NR=='$i'{print $9}' stats/.avg)
-      calc="${calc}+${x}"
-    done
-    calc="${calc}))/${line}"
-    python3 -c 'print("'$title' %cpu_utils : {0:.2f};".format('$calc'),end=" ")' >> stats/summary.log
-    
-    # todo
-    # perf stat -d -t $pid -o stats/.avg
-    # python3 -c 'print("'$title' ctx_switch : ",end=" ")' >> stats/summary.log
+      calc="((0"
+      for ((i=1; i <= $line; i++)); do
+        x=$(awk 'NR=='$i'{print $9}' stats/.avg)
+        calc="${calc}+${x}"
+      done
+      calc="${calc}))/${line}"
+      python3 -c 'print("'$title' %cpu_utils : {0:.2f};".format('$calc'),end=" ")' >> stats/summary.log
+
+    elif [[ $chk == "perf" ]]; then
+      perf stat -d -t $pid -o stats/.avg
+      ctx=$(grep "context-switches" stats/.avg | awk '{print $1}')
+      python3 -c 'print("'$title' ctx_switch : '$ctx';",end=" ")' >> stats/summary.log
+    fi
 
     prevpid=$pid
   fi
--- run.sh
+++ run.sh
@@ -1,32 +1,34 @@
 #! /usr/bin/env bash
 
-set -e
+set -x -e
 
 mkdir -p stats
 touch stats/.avg
 touch stats/.tp
-bash mon.sh &
+# check mode "top"|"perf"
+bash mon.sh top &
 pid=$!
 echo $pid
 # change below values
 duration=60
-for nrun in {1..3}; do echo -e "#run_${nrun}" >> stats/summary.log;
+for nrun in {1..1}; do echo -e "#run_${nrun}" >> stats/summary.log;
 for cc in {1,10}; do
 for c in {1,5,10,15,20}; do
 for r in {2,3,5,7}; do
-for v in {1,2}; do
+for v in {0,2}; do
 
   date
   title="${c}c1s${r}r1p_${cc}-con"
 
-  if [[ $v -eq 1 ]]; then
+  if [[ $v -eq 0 ]]; then
     title="${title}_broadcast"
   elif [[ $v -eq 2 ]]; then
     title="${title}_crpc-ring"
   fi
   echo -e "$title\n" > stats/.pid
-  time ./build/deptran_server -f config/sample_crpc.yml -f config/${c}c1s${r}r1p.yml -f config/rw.yml -f config/concurrent_${cc}.yml -P localhost -v $v -d $duration | tee -a stats/.pid | grep "tid of leader is "
+  time ./build/deptran_server -f config/test_toy.yml -f config/${c}c1s${r}r1p.yml -f config/rw.yml -f config/concurrent_${cc}.yml -P localhost -v $v -d $duration | tee -a stats/.pid | grep "tid of leader is "
   grep "all clients have shut down" stats/.pid
+  while [[ $(tail -n 1 stats/summary.log | grep $title | wc -l) -eq 0 ]]; do sleep 2; done
 
   grep Throughput stats/.pid > stats/.tp
   line=$c
@@ -39,7 +41,7 @@ for v in {1,2}; do
   python3 -c 'print("throughput : {0:.2f}".format('$calc'))' >> stats/summary.log
   tail -n 1 stats/summary.log
 
-  echo -e "\n\nrun #${nrun}\n" > stats/$title.log
+  echo -e "\n\n#run_${nrun}\n" >> stats/$title.log
   cat stats/.pid >> stats/$title.log
   cat stats/.avg >> stats/$title.log
   cat /dev/null > stats/.pid
