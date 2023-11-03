02852e1e54a5c14cc32f1c9b332f7df8d9ac6c99
florentinobenedictus <85059763+florentinobenedictus@users.noreply.github.com>
2023-11-03 21:50:22 +0700

    print leader tid

--- ctx_aggregate.sh
+++ ctx_aggregate.sh
@@ -1,18 +1,21 @@
 mkdir -p statsperfresult
+rm statsperfresult/aggregated_bc.log statsperfresult/aggregated_crpc.log statsperfresult/aggregated_summary.log
 
 for subdir in statsperfresult/statsperf_bc_*/; do
   subfolder_name=$(basename "$subdir")
-  echo "$subfolder_name :" >> statsperfresult/aggregate_bc.log
-  cat "$subdir/summary.log" >> statsperfresult/aggregate_bc.log
-  echo -e "\n" >> statsperfresult/aggregate_bc.log
+  leader_tid=$(awk '/tid of leader is [0-9]+/ {print $NF}' "$subdir/program_logs.txt")
+  echo "$subfolder_name, leader tid : $leader_tid" >> statsperfresult/aggregated_bc.log
+  cat "$subdir/summary.log" >> statsperfresult/aggregated_bc.log
+  echo -e "\n" >> statsperfresult/aggregated_bc.log
 done
 
 for subdir in statsperfresult/statsperf_crpc_*/; do
   subfolder_name=$(basename "$subdir")
-  echo "$subfolder_name :" >> statsperfresult/aggregate_crpc.log
-  cat "$subdir/summary.log" >> statsperfresult/aggregate_crpc.log
-  echo -e "\n" >> statsperfresult/aggregate_crpc.log
+  leader_tid=$(awk '/tid of leader is [0-9]+/ {print $NF}' "$subdir/program_logs.txt")
+  echo "$subfolder_name, leader tid : $leader_tid" >> statsperfresult/aggregated_crpc.log
+  cat "$subdir/summary.log" >> statsperfresult/aggregated_crpc.log
+  echo -e "\n" >> statsperfresult/aggregated_crpc.log
 done
 
-paste -d$'\t' statsperfresult/aggregate_crpc.log statsperfresult/aggregate_bc.log | column -s$'\t' -t > statsperfresult/aggregated_summary.log
+paste -d$'\t' statsperfresult/aggregated_crpc.log statsperfresult/aggregated_bc.log | column -s$'\t' -t > statsperfresult/aggregated_summary.log
 sed -i 's/Running for duration, Throughput: [0-9.]*$/&\n/g' statsperfresult/aggregated_summary.log
